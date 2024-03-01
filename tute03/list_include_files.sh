d541703d5f5d7ad750dc55b575c558c7b313996f
Hanan Hassan <h.hassan@student.unsw.edu.au>
2024-03-01 11:00:03 +1100

    tute03 working

--- tute03/list_include_files.sh
+++ tute03/list_include_files.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+for file in *.c
+do
+    if ! test -e "$file"; then
+        echo "$0 error: no .c files in the current directory" >&2
+        exit 1
+    fi
+
+    echo "$file includes:" 
+    grep -E '^#include' "$file" |
+    sed 's/[">][^">]*$//' |
+    sed 's/^.*["<]//'
+done
--- tute03/seq.sh
+++ tute03/seq.sh
@@ -1,1 +1,43 @@
 #! /usr/bin/env dash
+
+if test $# -eq 1; then
+    first=1
+    increment=1
+    last=$1
+elif test $# -eq 2; then
+    first=$1
+    increment=1
+    last=$2
+elif test $# -eq 3; then
+    first=$1
+    increment=$2
+    last=$3
+else
+    # error checking
+    echo "Usage: $0: [FIRST [INCREMENT]] LAST" >& 2
+    exit 1
+fi
+
+if test "$first" -eq "$first" 2> /dev/null; then
+    :
+else
+    echo "$0 Error: FIRST must be an integer" >& 2
+    exit 1
+fi
+# check other vairables for integers
+#
+if test "$first" -ge "$last" 2> /dev/null; then
+    echo "$0 error: FIRST must be less than LAST" >& 2
+    exit 1
+fi
+
+if ["$increment" -le 0] 2> /dev/null; then
+    echo "$0 error: INCREMENT must be positive" >& 2
+    exit 1
+fi
+
+cur="$first"
+while test "$cur" -le "$last"; do  
+    echo "$cur"
+    cur=$((cur + increment))
+done
